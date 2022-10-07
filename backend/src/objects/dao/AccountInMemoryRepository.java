16a228c41a96cb253c20104d85524bc41b931e60
waishun78 <64391406+waishun78@users.noreply.github.com>
2022-10-07 11:13:04 +0200

    AccountInMemoryRepo

--- backend/src/objects/dao/AccountInMemoryRepository.java
+++ backend/src/objects/dao/AccountInMemoryRepository.java
@@ -5,6 +5,7 @@ import objects.Account;
 import java.util.Collection;
 
 public class AccountInMemoryRepository {
+    //TODO: Might create an overall class that does findByGreaterTha for allrepository
 //    public Collection<Account> findByGreaterThan(long threshold){
 //    }
 }
