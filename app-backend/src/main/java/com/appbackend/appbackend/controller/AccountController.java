3bb05bd7da2c836cbc2467343755342f61ff045e
waishun78 <64391406+waishun78@users.noreply.github.com>
2022-11-26 23:15:40 +0100

    Migrated business action into business layer from controller layer

--- app-backend/src/main/java/com/appbackend/appbackend/controller/AccountController.java
+++ app-backend/src/main/java/com/appbackend/appbackend/controller/AccountController.java
@@ -1,67 +1,108 @@
 package com.appbackend.appbackend.controller;
 
+import com.appbackend.appbackend.business.AccountService;
 import com.appbackend.appbackend.exception.ResourceNotFoundException;
 import com.appbackend.appbackend.model.Account;
-import com.appbackend.appbackend.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @CrossOrigin
 @RestController
 @RequestMapping("api/v1/accounts")
 public class AccountController {
 
+    // @Autowired
+    // private AccountRepository accountRepository;
     @Autowired
-    private AccountRepository accountRepository;
+    protected AccountService accountService;
 
+    // @GetMapping
+    // public List<Account> getAllAccounts(){
+    //     return accountRepository.findAll();
+    // }
     @GetMapping
     public List<Account> getAllAccounts(){
-        return accountRepository.findAll();
+        return StreamSupport.stream(accountService.readAll().spliterator(), false).collect(Collectors.toList());
     }
 
     // create account REST API
     // TODO: Way to assign a username to the creation, instead of including the entire mapping into the account
+    // @PostMapping
+    // public Account createAccount(@RequestBody Account account){
+    //     return accountRepository.save(account);
+    // }
     @PostMapping
     public Account createAccount(@RequestBody Account account){
-        return accountRepository.save(account);
+        return accountService.create(account);
     }
 
     //get account by id REST API
+    // @GetMapping("{username}")
+    // public ResponseEntity<Account> getAccountById(@PathVariable String username){
+    //     Account account = accountRepository.findById(username)
+    //             .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee %s does not exist","username")));
+    //     return ResponseEntity.ok(account);
+    // }
     @GetMapping("{username}")
     public ResponseEntity<Account> getAccountById(@PathVariable String username){
-        Account account = accountRepository.findById(username)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee %s does not exist","username")));
+        Account account = accountService.readById(username).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return ResponseEntity.ok(account);
     }
 
     //update account REST API
+    // @PutMapping("{username}")
+    // public ResponseEntity<Account> updateAccount(@PathVariable String username, @RequestBody Account accountDetails){
+    //     Account accountToUpdate = accountRepository.findById(username)
+    //             .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee %s does not exist","username")));
+    //     //TODO: Move this to service layer
+    //     if (accountDetails.getAccountBalance() != null){
+    //         accountToUpdate.setAccountBalance(accountDetails.getAccountBalance());
+    //     }
+    //     if (accountDetails.getNotes() != null){
+    //         accountToUpdate.setNotes(accountDetails.getNotes());
+    //     }
+    //     accountRepository.save(accountToUpdate);
+    //     return ResponseEntity.ok(accountToUpdate);
+    // }
     @PutMapping("{username}")
     public ResponseEntity<Account> updateAccount(@PathVariable String username, @RequestBody Account accountDetails){
-        Account accountToUpdate = accountRepository.findById(username)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee %s does not exist","username")));
-        //TODO: Move this to service layer
-        if (accountDetails.getAccountBalance() != null){
-            accountToUpdate.setAccountBalance(accountDetails.getAccountBalance());
+        try{
+            Account accountToUpdate = accountService.readById(username).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+            if (accountDetails.getAccountBalance() != null){
+                accountToUpdate.setAccountBalance(accountDetails.getAccountBalance());
+            }
+            if (accountDetails.getNotes() != null){
+                accountToUpdate.setNotes(accountDetails.getNotes());
+            }
+            accountService.update(accountToUpdate);
+            return ResponseEntity.ok(accountToUpdate);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.notFound().build();
         }
-        if (accountDetails.getNotes() != null){
-            accountToUpdate.setNotes(accountDetails.getNotes());
-        }
-        accountRepository.save(accountToUpdate);
-        return ResponseEntity.ok(accountToUpdate);
     }
 
     // delete account REST API
+    // @DeleteMapping("{username}")
+    // public ResponseEntity<Account> deleteAccount(@PathVariable String username){
+    //     Account account = accountRepository.findById(username)
+    //             .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee %s does not exist","username")));
+
+    //     accountRepository.delete(account);
+
+    //     return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    // };
     @DeleteMapping("{username}")
     public ResponseEntity<Account> deleteAccount(@PathVariable String username){
-        Account account = accountRepository.findById(username)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee %s does not exist","username")));
-
-        accountRepository.delete(account);
-
+        accountService.deleteById(username);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     };
 }
---
+++ app-backend/src/main/java/com/appbackend/business/AbstractCrudService.java
@@ -0,0 +1,70 @@
+package com.appbackend.appbackend.business;
+
+/*
+ * Project Social Network. Created for Java Technology course at Czech Technical University in Prague,
+ * Faculty of Information Technology.
+ *
+ * Author: Ondřej Guth (ondrej.guth@fit.cvut.cz)
+ *
+ * This code is intended for educational purposes only.
+ */
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.appbackend.appbackend.model.DomainEntity;
+
+/**
+ * Common superclass for business logic of all entities supporting operations Create, Read, Update, Delete.
+ *
+ * @param <ID> Type of (primary) key.
+ * @param <E> Type of entity
+ */
+public abstract class AbstractCrudService<E extends DomainEntity<ID>, ID> {
+    /**
+     * Reference to data (persistence) layer.
+     */
+    protected final JpaRepository<E, ID> repository;
+
+    protected AbstractCrudService(JpaRepository<E, ID> repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Attempts to store a new entity. Throws exception if an entity with the same key is already stored.
+     *
+     * @param entity entity to be stored
+     * @throws EntityStateException if an entity with the same key is already stored
+     */
+    public E create(E entity) throws IllegalArgumentException {
+        if (repository.existsById(entity.getId()))
+            throw new IllegalArgumentException("entity " + entity + " already exists");
+        return repository.save(entity); //delegate call to data layer
+    }
+
+    public Optional<E> readById(ID id) {
+        return repository.findById(id);
+    }
+
+    public Iterable<E> readAll() {
+        return repository.findAll();
+    }
+
+    /**
+     * Attempts to replace an already stored entity.
+     *
+     * @param entity the new state of the entity to be updated; the instance must contain a key value
+     * @throws EntityStateException if the entity cannot be found
+     */
+    public E update(E entity) throws IllegalArgumentException {
+        if (repository.existsById(entity.getId()))
+            return repository.save(entity);
+        else
+            throw new IllegalArgumentException("entity " + entity + " does not exist exists");
+    }
+
+    public void deleteById(ID id) {
+        repository.deleteById(id);
+    }
+}
